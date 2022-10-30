ea5c491cc7fed7f0dbd1b7868c90dbd1f3f18812
Taher Fakhruddin Mukadam <mukadam.taher@gmail.com>
2022-10-30 20:26:48 +0300

    changed about file & master

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <!-- ======= Hero Section ======= -->
     <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
         <div class="hero-container" data-aos="fade-in">
-            <h1>Taher Mukadam</h1>
+            <h1>Taher</h1>
             <p>I'm <span class="typed" data-typed-items="Engineer,Developer, Freelancer, Photographer"></span></p>
         </div>
     </section><!-- End Hero -->
